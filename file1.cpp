54d47eed7a30e6d1a4c0137f4a376b88e10f697b
MuhammadRioAprilianto <rma994636@gmail.com>
2025-05-22 10:40:02 +0700

    membuat case 4 dari menu pilihan

--- file1.cpp
+++ file1.cpp
@@ -120,6 +120,11 @@ int main()
                 cout << "Stack is empty. No top value." << endl;
             }
             break;
+            
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
+        
         }
     }
 }
