d1830a4cb5a23cdbe7d36e6199d9f6c317fc3856
MuhammadRioAprilianto <rma994636@gmail.com>
2025-05-22 09:58:50 +0700

    membuat prosedur insert element

--- file1.cpp
+++ file1.cpp
@@ -18,5 +18,17 @@ class Stack{
 private:
 Node* top; //Pointer to the top node of the stack
 
+public:
+Stack(){
+    top = NULL; //Initialize the stack with a null top pointer
+}
 
+//push operation: insert an element onto the top of the stack
+int push(int value){
+    Node* newNode = new Node(); // 1. Allocate memory for the new node
+    newNode->data = value; //2. assign value
+    newNode->next = top; //3. set the next pointer of the new node to the current top
+    top = newNode;
+    cout << "Push value: " << value << endl;
+}
 };
